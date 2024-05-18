5f424e7ece68f9a840839acfb3aa492dfe0e4a58
Gabriel A. Sato <gwork1324@gmail.com>
2024-05-17 23:16:00 -0300

    Improving and decorating

--- exercise1/javascript/script.js
+++ exercise1/javascript/script.js
@@ -3,19 +3,18 @@ document.getElementById('textWelcome').style.display = 'none';
 function verifyEnter(event) {
     if (event.key === "Enter") {
         const nameUser = document.getElementById('inputUser').value;
-        const textWelcome = `Olá ${nameUser}, seja bem-vindo(a) ao mundo da programação!`;
+        const textWelcome = `Hello ${nameUser}, welcome to the world of programming!`;
         const welcomeElement = document.getElementById('textWelcome'); // Defina a variável welcomeElement
         welcomeElement.innerHTML = textWelcome;
 
         // Adicione a classe para iniciar a animação
         document.getElementById('inputUser').classList.add('fadeOut');
+        document.getElementById('textWelcome').classList.add('fadeIn');
 
         // Antes da animação, oculte o elemento
         setTimeout(() => {
             document.getElementById('inputUser').style.display = 'none';
         }, 500);
-
-        document.getElementById('textWelcome').classList.add('fadeIn');
 
         setTimeout(() => {
             document.getElementById('textWelcome').style.display = '';
