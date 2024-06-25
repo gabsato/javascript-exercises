0662ea94f444aa589f9d9855b556b76d5a23111e
Gabriel A. Sato <gwork1324@gmail.com>
2024-06-25 14:02:35 -0300

    exercise 4

---
+++ exercise4/script.js
@@ -0,0 +1,9 @@
+var number = prompt("Enter the number: ");
+
+if (number > 0) {
+    alert("The number is positive");
+} else if (number < 0) {
+    alert("The number is negative");
+} else {
+    alert("The number is zero");
+}
