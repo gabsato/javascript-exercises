c18b2d91f0931323bbe6a57cb88c14a5cac2ab7b
Gabriel A. Sato <gwork1324@gmail.com>
2024-06-26 16:48:49 -0300

    exercise 15

---
+++ exercise15/script.js
@@ -0,0 +1,7 @@
+var book = {};
+
+book.title = prompt("Enter the title of book: ");
+book.author = prompt("Enter the author name: ");
+book.year = prompt("Enter the year of publication: ");
+
+alert("Title: " + book.title + "\nAuthor: " + book.author + "\nYear: " + book.year);
